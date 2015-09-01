d04a0c4d57c0dacb910664fa70cf2c35e2140190
luoyuan800 <luoyuan800@163.com>
2015-09-01 22:48:16 +0800

    achievement
    home

--- src/cn/gavin/Achievement.java
+++ src/cn/gavin/Achievement.java
@@ -10,11 +10,24 @@ import java.util.List;
  */
 public enum Achievement {
     reBird("重生", "游戏中重置过一次", 0, 0, 0, 0),
-    click10000("点击中手", "点击次数达到10000次", 0, 0, 0, 2),
-    click50000("点击高手", "点击次数达到50000次", 0, 0, 0, 4),
-    click100000("点击达人", "点击次数达到100000次", 0, 0, 0, 6),
+    click10000("点击中手", "手抽筋了吧？点击次数达到10000次", 0, 0, 0, 2),
+    click50000("点击高手", "手抖都要点？点击次数达到50000次", 0, 0, 0, 4),
+    click100000("点击达人", "你是还是单身吧？点击次数达到100000次", 0, 0, 0, 6),
     click100("点击新手", "点击次数达到100次", 0, 0, 0, 0),
-    unbeaten("不败", "连胜次数达到100", 10, 0, 0, 0),
+    unbeaten("不败", "只能说你运气太好了，连胜次数达到100", 10, 0, 0, 0),
+    maze50("菜鸟", "到达迷宫50层", 0, 0, 0, 0),
+    maze100("新手", "到达迷宫100层", 0, 0, 20, 0),
+    maze500("不是新人", "到达迷宫500层", 0, 20, 0, 0),
+    maze1000("高手", "真难得，竟然到达迷宫1000层了", 20, 20, 20, 20),
+    maze10000("无敌", "到达迷宫1000层了……", -20, -20, -20, -20),
+    maze50000("无聊之人", "你真的好无聊，这么闷都玩到迷宫50000层了。", 1000, 1000, 1000, 1000),
+    richer("有钱人", "你好帅好有钱哦，成功内购一次。", 0, 0, 0, 5),
+    extreme("至尊", "偏科的典范，攻击力达到上限了", 0, 0, 0, 0),
+    master("至强", "偏科的典范，防御达到上限了", 0, 0, 0, 0),
+    lazy("懒惰", "持有点数5000点都不给分配，你是想怎么样玩这个游戏呢？", 0, 0, 0, 0),
+    linger("流连忘返", "这么烂的游戏你还玩第二遍……成功读取一次存档", 0, 0, 0, 0),
+    rich("富裕", "持有点数5000000点材料。", 0, 0, 0, 0),
+    speculator("投机者", "到达迷宫100层的时候武器和防具的等级均为0", 0, 0, 0, 0),
     EMPTY("", "", 0, 0, 0, 0);
     private int addStrength;
     private int addPower;
@@ -34,17 +47,19 @@ public enum Achievement {
         this.click = click;
     }
 
-    public void enable(){
+    public void enable() {
         this.enable = true;
     }
 
     public void enable(Hero hero) {
-        this.enable = true;
-        hero.addStrength(addStrength);
-        hero.addLife(addPower);
-        hero.addAgility(addAgility);
-        hero.addClickAward(click);
-        MainGameActivity.context.addMessage("-----------------","|获得成就：" + name() + "|", "-----------------");
+        if (!this.enable) {
+            this.enable = true;
+            hero.addStrength(addStrength);
+            hero.addLife(addPower);
+            hero.addAgility(addAgility);
+            hero.addClickAward(click);
+            MainGameActivity.context.addMessage("-----------------", "|获得成就：" + name() + "|", "-----------------");
+        }
     }
 
     public void disable(Hero hero) {
