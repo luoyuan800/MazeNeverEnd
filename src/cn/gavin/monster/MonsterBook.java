c8af4c330a1b4577adfa2784c63d3b67ec46bdad
Gavin <Gavin>
2015-09-08 17:47:35 +0800

    bigicon

---
+++ src/cn/gavin/monster/MonsterBook.java
@@ -0,0 +1,20 @@
+package cn.gavin.monster;
+
+import cn.gavin.activity.MainGameActivity;
+
+/**
+ * Created by gluo on 9/8/2015.
+ */
+public class MonsterBook {
+    private MainGameActivity context;
+    public MonsterBook(MainGameActivity context){
+        this.context = context;
+    }
+    public void showBook(){
+
+    }
+
+    public void addMonster(Monster monster){
+
+    }
+}
